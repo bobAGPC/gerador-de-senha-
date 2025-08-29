22a6088ce073c5718d40d1b238d27c5f73b7d5ea
DXP999 <142988042+bobAGPC@users.noreply.github.com>
2025-08-29 01:43:59 +0000

    28/08

---
+++ main.js
@@ -0,0 +1,5 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
